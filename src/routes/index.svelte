3bca75c5c6bc03798a49d842d6e0a9f9d53ae90a
PathonScript <pathonscript@gmail.com>
2022-07-31 19:57:32 +0700

    input and button for main page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,4 @@
-<h1 class="text-6xl">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="fullflex">
+	<input class="border border-black" type="text">
+	<button>Submit</button>
+</div>
